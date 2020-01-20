3531c95be7fa9edd619171f75da9ddbcc4e1f43a
Bryan Sills <bryan@bryansills.com>
2020-01-19 22:11:02 -0600

    Try some database stuff

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -24,6 +24,13 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:1.2.6")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("io.ktor:ktor-server-core:1.2.6")
+
+    implementation("com.h2database:h2:1.4.196")
+    implementation("org.jetbrains.exposed:exposed-core:0.18.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.18.1")
+    implementation("org.postgresql:postgresql:42.2.2")
+    implementation("com.zaxxer:HikariCP:3.4.1")
+
     testImplementation("io.ktor:ktor-server-tests:1.2.6")
 }
 
--- server/src/main/java/ninja/bryansills/pillow/server/PillowServer.kt
+++ server/src/main/java/ninja/bryansills/pillow/server/PillowServer.kt
@@ -5,14 +5,13 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
-import io.ktor.http.ParametersBuilder
-import io.ktor.http.URLBuilder
-import io.ktor.http.URLProtocol
-import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.util.toMap
+import java.net.URI
+import java.net.URISyntaxException
+import java.sql.*
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -24,5 +23,34 @@ fun Application.module(testing: Boolean = false) {
         get("/") {
             call.respondText("HELLO BUTTZ FROM BUILT DOCKER!", contentType = ContentType.Text.Plain)
         }
+        get("/database") {
+            call.respondText(databaseStuff(), contentType = ContentType.Text.Plain)
+        }
     }
+}
+
+@Throws(URISyntaxException::class, SQLException::class)
+private fun getConnection(): Connection {
+    val dbUri = URI(System.getenv("DATABASE_URL"))
+    val username: String = dbUri.userInfo.split(":").get(0)
+    val password: String = dbUri.userInfo.split(":").get(1)
+    val dbUrl = "jdbc:postgresql://" + dbUri.host + dbUri.path
+    return DriverManager.getConnection(dbUrl, username, password)
+}
+
+private fun databaseStuff(): String {
+    val connection = getConnection()
+
+    val stmt: Statement = connection.createStatement()
+    stmt.executeUpdate("DROP TABLE IF EXISTS ticks")
+    stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)")
+    stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+    val rs: ResultSet = stmt.executeQuery("SELECT tick FROM ticks")
+
+    var result = ""
+    while (rs.next()) {
+        result += rs.getTimestamp("tick")
+    }
+
+    return result
 }
