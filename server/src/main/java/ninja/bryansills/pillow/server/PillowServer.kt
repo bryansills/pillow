9eb8ab41f677afa15c91046461bf4782296de09e
Bryan Sills <bryan@bryansills.com>
2020-01-19 23:13:31 -0600

    Try something slightly different

--- server/src/main/java/ninja/bryansills/pillow/server/PillowServer.kt
+++ server/src/main/java/ninja/bryansills/pillow/server/PillowServer.kt
@@ -1,5 +1,7 @@
 package ninja.bryansills.pillow.server
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -8,9 +10,16 @@ import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.URI
 import java.net.URISyntaxException
 import java.sql.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -19,6 +28,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(DefaultHeaders)
+    initDb()
     routing {
         get("/") {
             println("YOYOYO ROOT")
@@ -27,6 +37,10 @@ fun Application.module(testing: Boolean = false) {
         get("/database") {
             println("UGHHHHHHHHHHHHHHHHHHHH")
             call.respondText(databaseStuff(), contentType = ContentType.Text.Plain)
+        }
+        get("/users") {
+            insert(UserDTO("FAKE", "NAMERSON", (0..69).random()))
+            call.respondText(getAllUsers().toString(), contentType = ContentType.Text.Plain)
         }
     }
 }
@@ -38,6 +52,9 @@ private fun getConnection(): Connection {
     val password: String = dbUri.userInfo.split(":")[1]
     val dbUrl = "jdbc:postgresql://" + dbUri.host + dbUri.path
     println("dbUrl: $dbUrl, username: $username, password: $password")
+    DriverManager.getDrivers().iterator().forEach {
+        println(it.toString())
+    }
     return DriverManager.getConnection(dbUrl, username, password)
 }
 
@@ -59,4 +76,68 @@ private fun databaseStuff(): String {
     println(result)
 
     return result
+}
+
+fun initDb() {
+    val dbUri = URI(System.getenv("DATABASE_URL"))
+    val username: String = dbUri.userInfo.split(":")[0]
+    val password: String = dbUri.userInfo.split(":")[1]
+    val dbUrl = "jdbc:postgresql://" + dbUri.host + dbUri.path
+
+    val config = HikariConfig()
+    config.jdbcUrl = dbUrl
+    config.username = username
+    config.password = password
+    config.dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
+
+    val ds = HikariDataSource(config)
+    Database.connect(ds)
+}
+
+object Users : Table() {
+    val id = uuid("id").primaryKey()
+    val firstname = text("firstname")
+    val lastname = text("lastname")
+    val age = integer("age")
+}
+
+data class User(
+        val id: UUID = UUID.randomUUID(),
+        val firstName: String,
+        val lastName: String,
+        val age: Int
+)
+
+data class UserDTO(
+        val firstName: String,
+        val lastName: String,
+        val age: Int
+)
+
+fun getAllUsers(): List<User> {
+    val users: ArrayList<User> = arrayListOf()
+    transaction {
+        Users.selectAll().map {
+            users.add(
+                    User(
+                            id = it[Users.id],
+                            firstName = it[Users.firstname],
+                            lastName = it[Users.lastname],
+                            age = it[Users.age]
+                    )
+            )
+        }
+    }
+    return users
+}
+
+fun insert(user: UserDTO) {
+    transaction {
+        Users.insert {
+            it[id] = UUID.randomUUID()
+            it[age] = user.age
+            it[firstname] = user.firstName
+            it[lastname] = user.lastName
+        }
+    }
 }
